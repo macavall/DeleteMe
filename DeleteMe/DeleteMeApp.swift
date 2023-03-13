c2b704620a38cb2ab4b07e6082a517dabceb09ff
Matt <matt.cavallo56@gmail.com>
2023-03-13 10:48:24 -0500

    Initial Commit

---
+++ DeleteMe/DeleteMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteMeApp.swift
+//  DeleteMe
+//
+//  Created by Matthew Cavallo on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DeleteMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
